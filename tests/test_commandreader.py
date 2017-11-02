a42fd240c14e46ad92e3722c01d271f0e215f8ef
Daniel B. Curtis <dbcurtis@dbcrd.net>
2017-11-01 18:50:36 -0700

    random

--- tests/test_commandreader.py
+++ tests/test_commandreader.py
@@ -3,10 +3,8 @@
 Test file for commandreader
 """
 from __future__ import print_function
-import os
 import sys
-from itertools import count
-import dlxii
+#from itertools import count
 import unittest
 import commandreader
 import userinput
@@ -25,7 +23,7 @@ class TestCommandreader(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        TestCommandreader.ui.inputfn="tests/cmdreadertest.txt"
+        TestCommandreader.ui.inputfn = "tests/cmdreadertest.txt"
 
 
 
@@ -34,61 +32,60 @@ class TestCommandreader(unittest.TestCase):
         pass
     
     def testopen(self):
-        cr=commandreader.CommandReader(TestCommandreader.ui)
+        _cr = commandreader.CommandReader(TestCommandreader.ui)
         fcr = None
-        try:              
-           
-            aa = cr.get()
-            self.assertTrue(cr.is_closed)
-            self.assertEqual("",aa)
-            cr.close()
-            self.assertTrue(cr.open())
-            self.assertFalse(cr.is_closed)
+        try:                       
+            self.assertTrue(_cr.is_closed)
+            self.assertEqual("", _cr.get())
+            _cr.close()
+            self.assertTrue(_cr.open())
+            self.assertFalse(_cr.is_closed)
             try:
-                cr.open()
-                self.assertTrue(False,"did not detect multiple attempts to open")
+                _cr.open()
+                self.assertTrue(False, "did not detect multiple attempts to open")
             except AssertionError:
                 pass
             
-            cr.close()
-            self.assertTrue(cr.is_closed)        
+            _cr.close()
+            self.assertTrue(_cr.is_closed)        
             fakeui = userinput.UserInput()
-            fakeui.inputfn='totaljunk.txt'
+            fakeui.inputfn = 'totaljunk.txt'
             fcr = commandreader.CommandReader(fakeui)
             self.assertFalse(fcr.open())
-            self.assertEqual("[Errno 2] No such file or directory: 'totaljunk.txt'",str( fcr.lasterror))
+            self.assertEqual("[Errno 2] No such file or directory: 'totaljunk.txt'", str(fcr.lasterror))
             self.assertTrue(fcr.is_closed)
         finally:           
             fcr.close()
-            cr.close()
+            _cr.close()
         
     
     def testclose(self):
         pass
     
     def testget(self):
-        cr=commandreader.CommandReader(TestCommandreader.ui)
+        _cr = commandreader.CommandReader(TestCommandreader.ui)
         try:
             
-            cr.open()
+            _cr.open()
             lines = []
             
             while True:
-                line = cr.get()
-                if line=="":break
+                line = _cr.get()
+                if line == "":
+                    break
                 lines.append(line)
                
-            cr.close()
-            self.assertEqual(7,len(lines))
+            _cr.close()
+            self.assertEqual(7, len(lines))
             lastline = lines[6]
             self.assertFalse(lastline.endswith("\n"))
             
             emptyline = lines[5]
-            self.assertEqual("\n",emptyline)
-            self.assertEqual("line 4\n",lines[3])
+            self.assertEqual("\n", emptyline)
+            self.assertEqual("line 4\n", lines[3])
         
         finally:
-            cr.close()
+            _cr.close()
 
 if __name__ == '__main__':
     unittest.main()
